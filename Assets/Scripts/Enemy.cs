cea348ee43c41c7903c81e261131efe1298e3c75
Onionie <gutierrez.onionie@gmail.com>
2021-03-04 23:56:06 -0800

    Enemy Health in Progress

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    private void OnTriggerEnter2d(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+    }   
+}
