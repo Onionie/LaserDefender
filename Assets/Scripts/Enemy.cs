a6f15d127570fa04f9dce1780e1c7bcc2b94c31f
Onionie <gutierrez.onionie@gmail.com>
2021-03-05 14:25:09 -0800

    Destroyable Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,9 +6,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
 
-    private void OnTriggerEnter2d(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        OnHit(damageDealer);
+
+    }
+
+    private void OnHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
-    }   
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
